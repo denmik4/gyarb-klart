db4283bfa09270d94a4cc937d52171929a1c3879
denmik4 <kira15302@gmail.com>
2023-02-07 10:48:37 +0100

    barrel fix

--- Scripts/MovableObjekt.gd
+++ Scripts/MovableObjekt.gd
@@ -4,18 +4,28 @@ class_name MovableObjekt
 var direction_x = "RIGHT"
 var velocity := Vector2.ZERO
 var direction := Vector2.ZERO
+var player = null
+var bug_fix = Vector2(1, 0)
 
 func _physics_process(delta: float) -> void:
-	velocity = move_and_slide(velocity)
+	if player:
+		if player.can_push and (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")) and player.state == 8:
+			if player.direction.x == 1: 
+				velocity = player.velocity - bug_fix
+				velocity.y = 0
+				move_and_slide(velocity)
+			if player.direction.x == -1:
+				velocity = player.velocity
+				velocity.y = 0
+				move_and_slide(velocity)
 
 
 
 func _on_Area2D_body_entered(body: Node) -> void:
 	if body.is_in_group("Player"):
-		var state = body.state
+		player = body
 		body.can_push = true
-		if body.can_push and (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")) and (state == body.PUSH_ACTIVE):
-			print(body.state)
+		if body.can_push and (Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right")):
 			velocity = body.velocity
 			velocity.y = 0
 		
@@ -24,6 +34,7 @@ func _on_Area2D_body_entered(body: Node) -> void:
 
 func _on_Area2D_body_exited(body: Node) -> void:
 	if body.is_in_group("Player"):
+		player = null
 		body.can_push = false
 		
 
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -17,6 +17,8 @@ var is_climb :bool
 var can_jump = true
 var can_push = false
 
+var offest = Vector2.ZERO
+
 onready var animationplayer = $AnimationPlayer
 
 func _physics_process(delta: float) -> void:
@@ -207,6 +209,7 @@ func _dodge_state(delta) -> void:
 		animationplayer.play("Jump_UP")
 		return
 		
+	can_jump = true
 	
 	if not Input.is_action_pressed("dodge"):
 		state = IDLE
@@ -242,6 +245,7 @@ func _dodge_run_state(delta) -> void:
 		state = ATTACK
 		return
 	
+	can_jump = true
 	
 	if Input.is_action_just_pressed("jump") and can_jump:
 		velocity.y = JUMP_STRENGHT
@@ -267,6 +271,7 @@ func _push_idle_state(delta) -> void:
 		state = ATTACK
 		return
 	
+	can_jump = true
 	
 	if Input.is_action_just_pressed("jump") and can_jump:
 		velocity.y = JUMP_STRENGHT
@@ -287,16 +292,22 @@ func _push_state(delta) -> void:
 	animationplayer.play("Push")
 	yield(get_tree().create_timer(0.20), "timeout")
 	
+	
+	
 	direction.x = _get_input_x_update_direction()
 	
 	if direction.x == 1:
-		velocity.x = move_toward(velocity.x, direction.x * MAX_SPEED, ACCELERATION*delta)
+		velocity.x = move_toward(velocity.x, direction.x * MAX_SPEED - 350, ACCELERATION*delta)
+		$Sprite.offset = Vector2(1.3,0)
 	elif direction.x == -1:
-		velocity.x = move_toward(velocity.x, direction.x * MAX_SPEED, ACCELERATION*delta)
+		velocity.x = move_toward(velocity.x, direction.x * MAX_SPEED + 350, ACCELERATION*delta)
+		$Sprite.offset = Vector2(-1.3,0)
 	else:
 		state = PUSH_IDLE
 		
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+	can_jump = true
 	
 	if can_push == false or Input.is_action_just_released("move_left") or Input.is_action_just_released("move_right"):
 		state = IDLE
@@ -304,23 +315,4 @@ func _push_state(delta) -> void:
 		
 		
 		
-
-
-
-""""
-func _on_MovableObjekt_push() -> void:
-	direction.x = _get_input_x_update_direction()
-	if direction.x == 1:
-		velocity.x = 0
-	elif direction.x == -1:
-		velocity.x = 0
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	state = PUSH
-	
-	if state == PUSH:
-		animationplayer.play("Push")
-		yield(get_tree().create_timer(0.20), "timeout")
-"""""
-
 
